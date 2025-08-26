5d98b1651d1f5719a867e73659706544ea316d77
chenwenjie <791476143@qq.com>
2025-08-26 16:40:32 +0800

    chore: 项目使用elementplus

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,65 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <el-container>
+    <el-header>
+      <el-row :gutter="20" align="middle" justify="center">
+        <el-col :span="12">
+          <div class="logo-container">
+            <a href="https://vite.dev" target="_blank">
+              <img src="/vite.svg" class="logo" alt="Vite logo" />
+            </a>
+            <a href="https://vuejs.org/" target="_blank">
+              <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+            </a>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
+    
+    <el-main>
+      <HelloWorld msg="Vite + Vue + TypeScript + Element Plus" />
+    </el-main>
+    
+    <el-footer>
+      <el-text type="info">
+        构建于 Vite + Vue 3 + TypeScript + Element Plus
+      </el-text>
+    </el-footer>
+  </el-container>
 </template>
 
 <style scoped>
+.el-header {
+  background-color: #f5f7fa;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+  padding: 20px 0;
+}
+
+.el-main {
+  background-color: #ffffff;
+  color: #333;
+  text-align: center;
+  min-height: calc(100vh - 180px);
+  padding: 20px;
+}
+
+.el-footer {
+  background-color: #f5f7fa;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.logo-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+}
+
 .logo {
   height: 6em;
   padding: 1.5em;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { Plus, Minus, Link, Document, Star } from '@element-plus/icons-vue'
 
 defineProps<{
   msg: string
@@ -9,35 +10,93 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <el-card class="box-card">
+    <template #header>
+      <div class="card-header">
+        <h1>{{ msg }}</h1>
+      </div>
+    </template>
+    
+    <div class="card-content">
+      <el-row :gutter="20" justify="center">
+        <el-col :span="12">
+          <el-button-group>
+            <el-button type="primary" @click="count++" :icon="Plus">
+              增加 {{ count }}
+            </el-button>
+            <el-button type="danger" @click="count > 0 && count--" :icon="Minus">
+              减少
+            </el-button>
+          </el-button-group>
+        </el-col>
+      </el-row>
+      
+      <el-divider></el-divider>
+      
+      <el-alert
+        title="开发提示"
+        type="info"
+        description="编辑 components/HelloWorld.vue 来测试热重载功能"
+        show-icon
+        :closable="false"
+      >
+      </el-alert>
+      
+      <el-divider></el-divider>
+      
+      <el-space direction="vertical" size="large">
+        <el-link href="https://vuejs.org/guide/quick-start.html#local" target="_blank" type="primary">
+          <el-icon><Link /></el-icon>
+          查看 create-vue 官方 Vue + Vite 启动模板
+        </el-link>
+        
+        <el-link 
+          href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" 
+          target="_blank" 
+          type="success"
+        >
+          <el-icon><Document /></el-icon>
+          了解更多 Vue 开发工具支持
+        </el-link>
+        
+        <el-link href="https://element-plus.org/" target="_blank" type="warning">
+          <el-icon><Star /></el-icon>
+          Element Plus 组件库文档
+        </el-link>
+      </el-space>
+    </div>
+  </el-card>
+  
+  <el-tag class="read-the-docs" type="info">
+    点击 Vite 和 Vue 图标了解更多
+  </el-tag>
 </template>
 
 <style scoped>
+.box-card {
+  width: 100%;
+  max-width: 600px;
+  margin: 20px auto;
+}
+
+.card-header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card-header h1 {
+  margin: 0;
+  color: #409eff;
+}
+
+.card-content {
+  padding: 20px 0;
+}
+
 .read-the-docs {
-  color: #888;
+  margin-top: 20px;
+  display: block;
+  text-align: center;
 }
 </style>
